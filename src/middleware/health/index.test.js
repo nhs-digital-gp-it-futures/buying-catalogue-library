d6ed88c29683cecd9bbafbe87f95c3d7b80e75a5
Fawad Ali <alibombaye@gmail.com>
2020-04-30 13:30:02 +0100

    WIP: mocking axios

--- src/middleware/health/index.test.js
+++ src/middleware/health/index.test.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import request from 'supertest';
+import axios from 'axios';
 import { healthRoutes } from './index';
+
+jest.mock('axios');
 
 describe('healthRoutes', () => {
   describe('/health/live', () => {
@@ -20,6 +23,27 @@ describe('healthRoutes', () => {
   });
 
   describe('/health/ready', () => {
+    describe('When there is only one dependency', () => {
+      it('should return "Healthy" if the response from the dependency is "Healthy"', () => {
+        axios.get.mockImplementationOnce(() => Promise.resolve('blah'));
+        
+        const app = express();
+        const router = express.Router();
+        app.use(router);
 
+        const dependencies = [{
+          name: 'some healthy dependency',
+          endpoint: 'http://some-endpoint',
+        }];
+
+        healthRoutes({ router, dependencies });
+
+        return request(app).get('/health/ready')
+          .expect(200)
+          .then((res) => {
+            expect(res.text).toBe('Healthy');
+          });
+      });
+    });
   });
 });
