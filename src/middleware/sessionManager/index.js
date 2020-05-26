280ca3c1a6ae2f380e0f8f9e7d36ec8e37553560
Fawad Ali <alibombaye@gmail.com>
2020-05-26 13:47:56 +0100

    add session managers

---
+++ src/middleware/sessionManager/index.js
@@ -0,0 +1,14 @@
+export const sessionManager = ({ logger }) => ({
+  saveToSession: ({ req, key, value }) => {
+    logger.debug(`Saving to session ${key} with value ${value}`);
+    req.session[key] = value;
+    logger.debug(`saved to session ${key} successfull`);
+  },
+
+  getFromSession: ({ req, key }) => {
+    logger.debug(`Getting from session ${key}`);
+    const valueFromSession = req.session[key];
+    logger.debug(`get from ${key} successfully`);
+    return valueFromSession;
+  },
+});
