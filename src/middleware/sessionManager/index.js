b4695058649038c4fe720eaa8069db30895b140e
Fawad Ali <alibombaye@gmail.com>
2020-05-26 14:11:16 +0100

    fix typos

--- src/middleware/sessionManager/index.js
+++ src/middleware/sessionManager/index.js
@@ -2,13 +2,13 @@ export const sessionManager = ({ logger }) => ({
   saveToSession: ({ req, key, value }) => {
     logger.debug(`Saving to session ${key} with value ${value}`);
     req.session[key] = value;
-    logger.debug(`saved to session ${key} successfull`);
+    logger.debug(`Saved to session ${key} successful`);
   },
 
   getFromSession: ({ req, key }) => {
     logger.debug(`Getting from session ${key}`);
     const valueFromSession = req.session[key];
-    logger.debug(`Get from ${key} successfull`);
+    logger.debug(`Get for ${key} successful`);
     return valueFromSession;
   },
 });
