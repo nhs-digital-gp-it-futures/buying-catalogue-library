f199ad62b0f6ff1d3ed615ce43fe687ef5c4c656
bjss-chris <christopher.martin@bjss.com>
2020-07-28 15:58:45 +0100

    Add stack trace to error context
    AB#8779

--- src/models/errorContext/index.js
+++ src/models/errorContext/index.js
@@ -5,11 +5,13 @@ export class ErrorContext {
     backLinkHref,
     title,
     description,
+    stackTrace,
   }) {
     this.status = status || 400;
     this.backLinkText = backLinkText || 'Go to Home Page';
     this.backLinkHref = backLinkHref || '/';
     this.title = title || 'Error';
     this.description = description || 'Something went wrong';
+    this.stackTrace = stackTrace;
   }
 }
