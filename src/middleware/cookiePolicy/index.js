b08f59b64918fcba717f153ee83aff208bb149c3
KeithLyonMastek <79255462+KeithLyonMastek@users.noreply.github.com>
2021-05-18 09:10:46 +0100

    cookiePolicyExists returning true/false (#114)
    * cookiePolicyExists returning true/false
    * cookie expiry date set to 1 year

--- src/middleware/cookiePolicy/index.js
+++ src/middleware/cookiePolicy/index.js
@@ -1,13 +1,15 @@
 const cookieName = 'buyingcatalogue-cookie-consent';
 
 export const cookiePolicyAgreed = ({ res, logger }) => {
-  res.cookie(cookieName, 'true', { maxAge: 360000 });
+  const millisecondsInOneYear = 31556926000;
+  res.cookie(cookieName, 'true', { maxAge: millisecondsInOneYear });
   logger.info(`creating ${cookieName} cookie`);
 };
 
 export const cookiePolicyExists = ({ req, logger }) => {
   const value = req.cookies[cookieName];
   logger.info(`get ${cookieName} cookie, has value ${value}`);
+  return value !== undefined;
 };
 
 export const cookiePolicyClear = ({ res, logger }) => {
