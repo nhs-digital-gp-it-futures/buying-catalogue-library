29f9306bcdd87bcbac6dbeecb00a8486ed1f1c13
bjss-chris <christopher.martin@bjss.com>
2021-05-28 17:09:17 +0100

    Do not delete consent cookie on logout
    AB#14926

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 const cookiePolicyDate = new Date('May 24, 2021 09:24:00');
+export const consentCookieName = 'buyingcatalogue-cookie-consent';
 
 module.exports = {
   // Cookie policy date
--- src/middleware/cookiePolicy/index.js
+++ src/middleware/cookiePolicy/index.js
@@ -1,6 +1,5 @@
 import config from '../../config';
 
-const cookieName = 'buyingcatalogue-cookie-consent';
 const currentTimeInMillisecods = new Date().getTime();
 
 export const cookiePolicyAgreed = ({ res, logger }) => {
@@ -9,26 +8,29 @@ export const cookiePolicyAgreed = ({ res, logger }) => {
     cookieValue: true,
     creationDate: new Date().getTime(),
   };
-  res.cookie(cookieName, `${JSON.stringify(cookieData)}`, { maxAge: millisecondsInOneYear });
+  res.cookie(config.consentCookieName, `${JSON.stringify(cookieData)}`, { maxAge: millisecondsInOneYear });
 
-  logger.info(`creating ${cookieName} cookie`);
+  logger.info(`creating ${config.consentCookieName} cookie`);
 };
 
 export const cookiePolicyExists = ({ req, logger }) => {
-  const value = req.cookies[cookieName];
-  logger.info(`get ${cookieName} cookie, has value ${value}`);
+  const value = req.cookies[config.consentCookieName];
+  logger.info(`get ${config.consentCookieName} cookie, has value ${value}`);
   return value !== undefined;
 };
 
 export const cookiePolicyClear = ({ res, logger }) => {
-  res.clearCookie(cookieName);
-  logger.info(`clear ${cookieName} cookie`);
+  res.clearCookie(config.consentCookieName);
+  logger.info(`clear ${config.consentCookieName} cookie`);
 };
 
 export const consentCookieExpiration = ({ router, logger }) => {
   if (config.buyingCatalogueCookiePolicyDate < currentTimeInMillisecods) {
     router.use((req, res, next) => {
-      const value = req.cookies[cookieName] ? JSON.parse(req.cookies[cookieName]) : undefined;
+      const value = req.cookies[config.consentCookieName]
+        ? JSON.parse(req.cookies[config.consentCookieName])
+        : undefined;
+
       if (value && value.creationDate && (
         value.creationDate < config.buyingCatalogueCookiePolicyDate
       )) {
